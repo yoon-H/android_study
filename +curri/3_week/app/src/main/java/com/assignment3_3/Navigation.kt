0ca374f5d7021440533bec3db007d87519888c2a
yoon-H <ucando0320@gmail.com>
2021-10-11 20:37:14 +0900

    Navigation 예습 - NavHost는 fragment들이 들어오는 자리, NavController로 네비게이션을 관리한다. - build.gradle(:app) 파일에 Compose 네비게이션 의존성을 추가한다. - route로 열 fragment를 지정하고, NavHost 안에 composable로  화면 전환을 지정한다. 이때, 입력된 name을 navArgument로 받아 DetailScreen에 넘긴다. - MainScreen에는 Textfield 로 name을 받고 버튼을 누르면 DetailScreen에선 "Hello (name)" 을 출력한다.
    + 2번 공부에서는 xml을 통해 Navigation graph를 그려 경로를 지정한다.

---
+++ +curri/3_week/app/src/main/java/com/assignment3_3/Navigation.kt
@@ -0,0 +1,81 @@
+package com.assignment3_3
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.DetailScreen.route + "/{name}",
+            arguments = listOf(
+                navArgument("name"){
+                    type = NavType.StringType
+                    defaultValue = "Philipp"
+                    nullable = true
+                }
+            )
+        ){entry ->
+            DetailScreen(name = entry.arguments?.getString("name"))
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(
+            value = text, onValueChange = {
+                text = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                navController.navigate(Screen.DetailScreen.withArgs(text))
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To DetailScreen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(name: String?) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Hello $name")
+    }
+}
---
+++ +curri/3_week/app/src/main/java/com/assignment3_3/Screen.kt
@@ -0,0 +1,15 @@
+package com.assignment3_3
+
+sealed class Screen(val route: String){
+    object MainScreen : Screen("main_screen")
+    object DetailScreen : Screen("datail_screen")
+
+    fun withArgs(vararg args: String): String{
+        return buildString {
+            append(route)
+            args.forEach {arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
